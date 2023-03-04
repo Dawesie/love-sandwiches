1e2b876367bb54385ad4554857977f4ebc7cae64
Dawesie <marie.dawe01967@gmail.com>
2023-03-04 10:57:03 +0000

    Add function to update sales figures in sales worksheet.

--- run.py
+++ run.py
@@ -16,6 +16,9 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     """
     Get sales figures input from the user.
+    Run a while loop to collect valid data from the user
+    vial that terminal, which must be a string of 6 numbers separated
+    by a commas.  The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -26,9 +29,9 @@ def get_sales_data():
         sales_data = data_str.split(",")
 
         if validate_data(sales_data):
-            print("Data is valid")
+            print("Data is valid!")
             break
-
+    return sales_data
 
 def validate_data(values):
     """
@@ -47,4 +50,15 @@ def validate_data(values):
         return False
     return True
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided.
+    """
+    print("updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
